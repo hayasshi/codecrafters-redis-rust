71e13bb5a6e18f33c71598531e92fa2124be1370
hayasshi <daisuke0884@gmail.com>
2023-01-06 17:21:12 +0900

    Support PING command

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 // Uncomment this block to pass the first stage
-use std::net::TcpListener;
+use std::{net::TcpListener, io::{Read, Write}};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -11,8 +11,13 @@ fn main() {
     
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut _stream) => {
                 println!("accepted new connection");
+                let mut buffer = String::new();
+                _stream.read_to_string(&mut buffer).unwrap();
+                if buffer.as_str() == "PING" {
+                    _stream.write("+PONG\r\n".as_bytes()).unwrap();
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
